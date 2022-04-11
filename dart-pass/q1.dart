402e4e812c3bd4916b56a0c997fbe6400234663e
Ali Adil <95132934+AliQ2@users.noreply.github.com>
2022-04-11 22:57:05 +0300

    Create q1.dart

---
+++ dart-pass/q1.dart
@@ -0,0 +1,13 @@
+/*
+the wrong in code because 
+when create object of DateTime class sholud not be constant
+becuse time vraing 
+
+correct code: 
+
+main(){
+  int hour = DateTime.now().hour;
+  print(hour);
+}
+
+*/
